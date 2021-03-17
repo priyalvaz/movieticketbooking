7cf7d3123e2f75f9deb30e86556277417db3abf1
priyalvaz <67680074+priyalvaz@users.noreply.github.com>
2021-03-17 22:22:39 +0530

    Add files via upload

---
+++ book/kin.php
@@ -0,0 +1,367 @@
+
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="book\book2.css"/>
+    <style>
+        @import url('https://fonts.googleapis.com/css?family=Lato&display=swap');
+
+
+body {
+  
+  color: #fff;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  font-family: 'Lato', sans-serif;
+  margin: 0;
+  margin: 0 auto;
+    background-image: url("../images/pic1.jpg");
+    background-repeat: no-repeat;
+    background-size: 100% 720px;
+ 
+  
+}
+
+.movie-container {
+  margin: 20px 0;
+}
+
+.movie-container select {
+  background-color: #fff;
+  border: 0;
+  border-radius: 5px;
+  font-size: 14px;
+  margin-left: 10px;
+  padding: 5px 15px 5px 15px;
+  -moz-appearance: none;
+  -webkit-appearance: none;
+  appearance: none;
+}
+
+.container {
+  perspective: 1000px;
+  margin-bottom: 30px;
+}
+
+.seat {
+  background-color: #444451;
+  height: 12px;
+  width: 15px;
+  margin: 3px;
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+}
+
+.seat.selected {
+  background-color: #6feaf6;
+}
+
+.seat.occupied {
+  background-color: #fff;
+}
+
+.seat:nth-of-type(2) {
+  margin-right: 18px;
+}
+
+.seat:nth-last-of-type(2) {
+  margin-left: 18px;
+}
+
+.seat:not(.occupied):hover {
+  cursor: pointer;
+  transform: scale(1.2);
+}
+
+.showcase .seat:not(.occupied):hover {
+  cursor: default;
+  transform: scale(1);
+}
+
+.showcase {
+  background: rgba(0, 0, 0, 0.1);
+  padding: 5px 10px;
+  border-radius: 5px;
+  color: #777;
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+}
+
+.showcase li {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0 10px;
+}
+
+.showcase li small {
+  margin-left: 2px;
+}
+
+.row {
+  display: flex;
+}
+
+.screen {
+  background-color: #fff;
+  height: 70px;
+  width: 100%;
+  margin: 15px 0;
+  transform: rotateX(-45deg);
+  box-shadow: 0 3px 10px rgba(255, 255, 255, 0.7);
+}
+
+p.text {
+  margin: 5px 0;
+}
+
+p.text span {
+  color: #6feaf6;
+}
+
+
+.boxitem{
+  width: 600px;
+  height: 600px;  
+  padding: 50px;
+  border: 5px solid red;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #05186d;
+  font-family: 'Lato', sans-serif;
+  margin: 0;
+}
+
+.button1 {
+  padding: 12px 40px;
+  font-size: 24px;
+  text-align: center;
+  cursor: pointer;
+  outline: none;
+  border: none;
+  border-radius: 7px;
+
+}
+.button1:hover {background-color: #8a80ad}
+.button1:active {
+  transform: translateY(2px);
+}
+
+
+
+    </style>
+    
+    <title>Movie Seat Booking</title>
+  </head>
+  <body>
+    <div class="boxitem">
+    <div class="movie-container">
+    <div class="form-input">
+                <label style="margin-right:73px;">Date</label>
+				<input type="date" name="dob" placeholder="Enter Date Of Birth" required/>
+            </div><br>
+            <label for="age1">Select Show Time : </label>&nbsp;&nbsp;
+            <input type="radio" id="age1" name="age">
+  <label >10AM</label>&nbsp;&nbsp;
+  <input type="radio" id="age2" name="age" >
+  <label >3PM</label> &nbsp;&nbsp;
+  <input type="radio" id="age3" name="age">
+  <label >7PM</label>&nbsp;&nbsp; 
+  <input type="radio" id="age3" name="age">
+  <label >9PM</label> <br><br>
+
+      <label style="margin-right:52px;"> Movie</label>
+      <select id="movie">
+        <option value="12">KIN ($12)</option>
+      </select>
+    </div>
+
+
+    <ul class="showcase">
+      <li>
+        <div class="seat"></div>
+        <small>N/A</small>
+      </li>
+      <li>
+        <div class="seat selected"></div>
+        <small>Selected</small>
+      </li>
+      <li>
+        <div class="seat occupied"></div>
+        <small>Occupied</small>
+      </li>
+    </ul>
+
+    <div class="container">
+      <div class="screen"></div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat occupied"></div>
+        <div class="seat occupied"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat occupied"></div>
+        <div class="seat occupied"></div>
+      </div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat occupied"></div>
+        <div class="seat occupied"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+      </div>
+
+      <div class="row">
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat"></div>
+        <div class="seat occupied"></div>
+        <div class="seat occupied"></div>
+        <div class="seat occupied"></div>
+        <div class="seat"></div>
+      </div>
+    </div>
+<form method="post">
+    <p class="text">
+      You have selected <span id="count">0</span> seats for a price of $
+     <span
+        id="price" name="price"
+        >0</span>
+    </p>
+  
+    <div style="text-align: center; margin-top: 20px;">
+            <button  formaction="../home.html" class="button1" style="background-color:black;color: white;border: 3px solid #f44336; margin-right: 30px; ">
+                Back</button>
+                <button  formaction="../payment.php" name="submit"class="button1" style="background-color:black;color: white;border: 3px solid #f44336; ">
+                   Book Now</button>
+          </div>
+
+
+        
+    <script >
+        const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const price = document.getElementById('price');
+
+const movieSelect = document.getElementById('movie');
+let ticketPrice = +movieSelect.value;
+
+const populateUI = () => {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  const selectedMoviePrice = localStorage.getItem('selectedMoviePrice');
+
+  if (selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex;
+  }
+
+  if (selectedMoviePrice !== null) {
+    count.innerText = selectedSeats.length;
+    price.innerText = selectedSeats.length * +selectedMoviePrice;
+  }
+};
+
+populateUI();
+
+selectedMovie = (movieIndex, moviePrice) => {
+  localStorage.setItem('selectedMovieIndex', movieIndex);
+  localStorage.setItem('selectedMoviePrice', moviePrice);
+};
+
+const updateSelectedSeatsCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .selected');
+
+  const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  price.innerText = selectedSeatsCount * ticketPrice;
+};
+
+// Seat select event
+container.addEventListener('click', e => {
+  if (
+    e.target.classList.contains('seat') &&
+    !e.target.classList.contains('occupied')
+  ) {
+    e.target.classList.toggle('selected');
+
+    updateSelectedSeatsCount();
+  }
+});
+
+// Movie select event
+movieSelect.addEventListener('change', e => {
+  ticketPrice = +e.target.value;
+  selectedMovie(e.target.selectedIndex, e.target.value);
+
+  updateSelectedSeatsCount();
+});
+
+    </script>
+    </form>
+    </div>
+  </body>
+</html>
